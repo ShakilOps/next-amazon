8ff4524b12a88966c94eb71245bc6c7eca6a3e9c
Shakil Ops <shakildo.7887@gmail.com>
2024-01-07 17:38:18 +0600

    placeorder

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from 'next/link'
 
-export default function ProductItem({product}) {
+export default function ProductItem({product, addToCartHandler}) {
   return (
     <div className='card'>
         <Link href={`/product/${product.slug}`}>
@@ -13,7 +13,11 @@ export default function ProductItem({product}) {
             </Link>
             <p className='mb-2'>{product.brand}</p>
             <p>${product.price}</p>
-            <button className='primary-button' type='button'>
+            <button
+                className="primary-button"
+                type="button"
+                onClick={() => addToCartHandler(product)}
+            >
                 Add to Cart
             </button>
         </div>
--- utils/Store.js
+++ utils/Store.js
@@ -6,11 +6,12 @@ export const Store = createContext();
 const initialState = {
   cart: Cookies.get('cart')
     ? JSON.parse(Cookies.get('cart'))
-    : { cartItems: [] },
+    : { cartItems: [], shippingAddress: {}, paymentMethod: '' },
 };
 
 function reducer(state, action) {
   switch (action.type) {
+
     case 'CART_ADD_ITEM': {
       const newItem = action.payload;
       const existItem = state.cart.cartItems.find(
@@ -24,6 +25,7 @@ function reducer(state, action) {
         Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
+
     case 'CART_REMOVE_ITEM': {
         const cartItems = state.cart.cartItems.filter(
           (item) => item.slug !== action.payload.slug
@@ -31,6 +33,7 @@ function reducer(state, action) {
         Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }));
         return { ...state, cart: { ...state.cart, cartItems } };
       }
+
       case 'CART_RESET':
         return {
           ...state,
@@ -40,6 +43,30 @@ function reducer(state, action) {
             paymentMethod: '',
           },
         };
+        
+        case 'CART_CLEAR_ITEMS':
+          return { ...state, cart: { ...state.cart, cartItems: [] } };
+
+        case 'SAVE_SHIPPING_ADDRESS':
+          return {
+            ...state,
+            cart: {
+              ...state.cart,
+              shippingAddress: {
+                ...state.cart.shippingAddress,
+                ...action.payload,
+              },
+            },
+          };
+          
+          case 'SAVE_PAYMENT_METHOD':
+            return {
+              ...state,
+              cart: {
+                ...state.cart,
+                paymentMethod: action.payload,
+              },
+            };
     default:
       return state;
   }
